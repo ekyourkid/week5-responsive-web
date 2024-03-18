a700e5bba110a876cdae77ec153234138f31e288
ekyourkid <eky1579@gmail.com>
2024-03-19 00:00:32 +0700

    fix: done task front beginner

--- profile/editProfile.js
+++
@@ -1,6 +0,0 @@
-const showPopup = document.querySelector(".btn-nav-logout");
-const popUpContainer = document.querySelector(".popUp-container");
-
-showPopup.onclick = () => {
-    popUpContainer.classList.add("active");
-};
